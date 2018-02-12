11a0590d67459d9f55bd4b45b689458126a3e0f3
mingmingrr <mingmingrr@gmail.com>
2018-02-11 23:33:50 -0500

    Add PC register

---
+++ sv/Pc/Pc_Action.v
@@ -0,0 +1,14 @@
+`ifndef PC_ACTION_M
+`define PC_ACTION_M
+
+`define Pc_Action_T(T) T [1:0]
+`define Pc_Action_W 2
+`define Pc_Action_L 4
+
+`define Pc_Action_None   2'h0
+`define Pc_Action_Inc    2'h1
+`define Pc_Action_Offset 2'h2
+`define Pc_Action_Jump   2'h3
+
+`endif
+
---
+++ sv/Pc/Pc_pc.v
@@ -0,0 +1,46 @@
+`ifndef PC_PC_I
+`define PC_PC_I
+
+`include "../Util/Util_Math.v"
+`include "../Util/Util_Control.v"
+`include "../Pc/Pc_Action.v"
+
+module Pc_pc #
+	( parameter ADDR_W = 32
+	, parameter OFFSET_W = 16
+	, parameter JUMP_W = 22
+	, parameter STEP = 4
+	, parameter SKIP = Util_Math_log2(STEP)
+	, parameter RESET = ADDR_W'(0)
+	)
+	( `Util_Control_T(input) ctrl
+	, `Pc_Action_T(input) act
+	, input [OFFSET_W-1:0] offset
+	, input [JUMP_W-1:0] jump
+	, output [ADDR_W-1:0] addr
+	);
+
+`Util_Math_log2_expr
+
+reg [ADDR_W-1:0] pc;
+assign addr = pc;
+
+wire signed [OFFSET_W-1:0] offset$;
+assign offset$ = offset;
+
+always @(posedge `Util_Control_clock(ctrl))
+	if(`Util_Control_reset(ctrl))
+		pc <= RESET;
+	else
+		case(act)
+			`Pc_Action_None   : pc <= pc;
+			`Pc_Action_Inc    : pc <= pc + STEP;
+			`Pc_Action_Offset : pc <= pc + (ADDR_W'(offset$) << SKIP);
+			`Pc_Action_Jump   : pc <= {pc[ADDR_W-1:JUMP_W+SKIP], jump, SKIP'(0)};
+			default : pc <= pc;
+		endcase
+
+endmodule
+
+`endif
+
