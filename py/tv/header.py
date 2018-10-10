92058cf7dc8ecd6a91e64063cc7da8f17252ce43
mingmingrr <mingmingrr@gmail.com>
2018-10-10 10:55:35 -0400

    Fix expander3 relative imports

--- py/tv/header.py
+++ py/tv/header.py
@@ -1,5 +1,5 @@
 $py(
 import importlib
 tv = importlib.machinery.SourceFileLoader('*',
-  '/home/mingmingrr/Workspace/EEL4713/Lab1/py/tv/__init__.py').load_module()
+  'py/tv/__init__.py').load_module()
 )
